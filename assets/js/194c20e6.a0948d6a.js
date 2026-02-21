159809d5217af472165f4dc81e73be0fc3e03bf3
Parul Singh <parul0909singh@gmail.com>
2026-02-21 15:29:08 +0000

    Deploy website - based on 2ffa9c3db4113330fc4d8622846e6239cb48d873

---
+++ assets/js/194c20e6.a0948d6a.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocs=globalThis.webpackChunkdocs||[]).push([[6797],{5012(o){o.exports=JSON.parse('{"metadata":{"permalink":"/notepad/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
